de0e8585844296764ae01980c2cbb40783debb96
Zahar Bystrok <zbystrok.jr@gmail.com>
2020-10-09 06:08:27 +0300

    init: start morse-decoder-task

--- src/index.js
+++ src/index.js
@@ -38,8 +38,49 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let str = ''
+    for (let i = 0; i < expr.length; i++) {
+      if (expr[i] == '*') {
+        if (expr[i + 1] != '*') {
+          str += ',!!!'
+        } else {
+          continue
+        }
+      }
+      if (i % 10 == 0 && i != 0) {
+          str += ','
+      } 
+      if (expr[i] == 1) {
+          if (expr[i + 1] == 0) {
+            str += '.'
+          } else {
+            str += '-'
+          }
+          i++
+        }
+      }
+    
+    console.log(str);
+    let arr = str.split(',')
+    console.log(arr);
+    let strEnd = ''
+        for (let i = 0; i < arr.length; i++) {
+          if (arr[i] == '!!!') {
+            strEnd += ' '
+          } else {
+            strEnd += MORSE_TABLE[arr[i]]
+          }
+        }
+   return strEnd
 }
+
+
+
+
+
+
+
+
 
 module.exports = {
     decode
